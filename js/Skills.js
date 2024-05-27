f580f949c977028d7be81283b3ab9fb9267120a5
Chukwuma Charleson Onwuka <onwuka.charles@yahoo.com>
2024-05-27 09:10:36 +0100

    Rearranged skills section

--- js/Skills.js
+++ js/Skills.js
@@ -6,12 +6,12 @@ function Skills() {
             <div class="skillsGrid">
                 ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original-wordmark.svg', alt: 'The logo icon for Azure', title: 'Azure' })}
                 ${window.Skill({ source: '../media/assets/LangchainLogo.png', alt: 'The logo icon for Langchain', title: 'Langchain' })}
-                ${window.Skill({ source: '../media/assets/OpenAI_Logo.png', alt: 'The logo icon for OpenAI', title: 'Open AI' })}
                 ${window.Skill({ source: '../media/assets/hf-logo.svg', alt: 'The logo icon for HuggingFace', title: 'HuggingFace' })}
-                ${window.Skill({ source: '../media/assets/pinecone_logo.png', alt: 'The logo icon for Pinecone', title: 'Pinecone' })}
                 ${window.Skill({ source: '../media/assets/chroma.svg', alt: 'The logo icon for Chroma DB', title: 'Chroma' })}
                 ${window.Skill({ source: '../media/assets/UiPath.svg', alt: 'The logo icon for UiPath', title: 'UI Path' })}
                 ${window.Skill({ source: '../media/assets/Blueprism.svg', alt: 'The logo icon for BluePrism', title: 'Blueprism' })}
+                ${window.Skill({ source: '../media/assets/OpenAI_Logo.png', alt: 'The logo icon for OpenAI', title: 'Open AI' })}
+                ${window.Skill({ source: '../media/assets/pinecone_logo.png', alt: 'The logo icon for Pinecone', title: 'Pinecone' })}
                 ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg', alt: 'The logo icon for Python', title: 'Python' })}
                 ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytorch/pytorch-original-wordmark.svg', alt: 'The logo icon for Pytorch', title: 'PyTorch 5' })}
                 ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/opencv/opencv-original-wordmark.svg', alt: 'The logo icon for Open CV', title: 'Open CV' })}
