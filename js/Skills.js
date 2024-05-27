c25b17c43f84ae7842a43aba10c175435487d8b9
Chukwuma Charleson Onwuka <onwuka.charles@yahoo.com>
2024-05-27 08:48:44 +0100

    Added RPA skills

--- js/Skills.js
+++ js/Skills.js
@@ -4,14 +4,22 @@ function Skills() {
         <div class="skills">
             <h2>I have experience with these technologies</h2>
             <div class="skillsGrid">
-                ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original-wordmark.svg', alt: 'The logo icon for react', title: 'Azure' })}
-                ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytorch/pytorch-original-wordmark.svg', alt: 'The logo icon for HTML 5', title: 'PyTorch 5' })}
-                ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tensorflow/tensorflow-original-wordmark.svg', alt: 'The logo icon for CSS3', title: 'Tensorflow' })}
+                ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original-wordmark.svg', alt: 'The logo icon for Azure', title: 'Azure' })}
+                ${window.Skill({ source: '../media/assets/LangchainLogo.png', alt: 'The logo icon for Langchain', title: 'Langchain' })}
+                ${window.Skill({ source: '../media/assets/OpenAI_Logo.png', alt: 'The logo icon for OpenAI', title: 'Open AI' })}
+                ${window.Skill({ source: '../media/assets/hf-logo.svg', alt: 'The logo icon for HuggingFace', title: 'HuggingFace' })}
+                ${window.Skill({ source: '../media/assets/pinecone_logo.png', alt: 'The logo icon for Pinecone', title: 'Pinecone' })}
+                ${window.Skill({ source: '../media/assets/chroma.svg', alt: 'The logo icon for Chroma DB', title: 'Chroma' })}
+                ${window.Skill({ source: '../media/assets/UiPath.svg', alt: 'The logo icon for UiPath', title: 'UI Path' })}
+                ${window.Skill({ source: '../media/assets/Blueprism.svg', alt: 'The logo icon for BluePrism', title: 'Blueprism' })}
+                ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytorch/pytorch-original-wordmark.svg', alt: 'The logo icon for Pytorch', title: 'PyTorch 5' })}
+                ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tensorflow/tensorflow-original-wordmark.svg', alt: 'The logo icon for Tensorflow', title: 'Tensorflow' })}
                 ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg', alt: 'The logo icon for Git', title: 'Git' })}
                 ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg', alt: 'The logo icon for Python', title: 'Python' })}
                 ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original-wordmark.svg', alt: 'The logo icon for Pandas', title: 'Pandas' })}
                 ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original-wordmark.svg', alt: 'The logo icon for Numpy', title: 'Numpy' })}
                 ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/opencv/opencv-original-wordmark.svg', alt: 'The logo icon for Open CV', title: 'Open CV' })}
+                ${window.Skill({ source: '../media/assets/microsoft_powerbi.svg', alt: 'The logo icon for Power BI', title: 'Power BI' })}
                 ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg', alt: 'The logo icon for My SQL', title: 'SQL' })}
                 ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/matplotlib/matplotlib-original-wordmark.svg', alt: 'The logo icon for MatplotLib', title: 'Matplotlib' })}
                 ${window.Skill({ source: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original-wordmark.svg', alt: 'The logo icon for FastAPI', title: 'Fast API' })}
